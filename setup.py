eca7f0224d1c857c9566f3d142b42161587b8317
Ankit Popli <ankitpopli1891@users.noreply.github.com>
2024-08-23 14:01:12 +0530

    Release v1.3.0

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ requirements = [getattr(ir, 'requirement', str(getattr(ir, 'req', None))) for ir
 
 setup(
     name='django-autotranslate',
-    version='1.2.0',
+    version='1.3.0',
     packages=find_packages(),
     install_requires=requirements,
     include_package_data=True,
