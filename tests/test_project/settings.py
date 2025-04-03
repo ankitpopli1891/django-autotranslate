bc2108fd2d021097ebc37310bb38cace7cb94d0c
Brian Kohan <bckohan@gmail.com>
2025-04-02 20:47:52 -0700

    fix tests (#48)

--- tests/test_project/settings.py
+++ tests/test_project/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'autotranslate',
 ]
 
-MIDDLEWARE_CLASSES = [
+MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
--- tests/test_project/urls.py
+++ tests/test_project/urls.py
@@ -14,9 +14,9 @@ Including another URLconf
     2. Import the include() function: from django.conf.urls import url, include
     3. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
-from django.conf.urls import url
+from django.urls import path
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    path(r'admin', admin.site.urls),
 ]
